0d86585dee9a8e485d32a8bf070bf862cc637da5
parinrava <pravanbakhsh@my.bcit.ca>
2023-05-14 16:13:38 -0700

    create pokemon card for each and display picture of each pokemon character

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 
 
-const setup = async () =>{
+const setup = async () => {
     console.log("jquery is working");
-   const result = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810');
-  const pokemons = result.data.results;
-  pokemons.forEach(pokemon => 
-    {
-    $("#main").append(`<div class="pokemon">${pokemon.name}</div>`);
-  });
+    const result = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810');
+    const pokemons = result.data.results;
+    pokemons.forEach((pokemon, index) => {
+        $("#main").append(`<div class="card" style="width: 18rem;">
+        <img src="https://raw.githubusercontent.com/pokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index + 1}.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${pokemon.name}</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>`);
+    });
 }
 $(document).ready(setup)
